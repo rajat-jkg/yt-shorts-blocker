d499b7c7f78b95951c9d0d58ad9fdf6c8636dcfc
Rajat <RajatJKG33@gmail.com>
2024-02-10 10:55:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# YT Shorts Blocker chrome extention
+
+This is a chrome extension that blocks youtobe shorts from loading.
+It insterts simple scripts in in the webpages when you activate this extention.
---
+++ index.js
@@ -0,0 +1,51 @@
+let shortsTab = document.getElementsByTagName('ytd-guide-entry-renderer')[1];
+let activeUrl = window.location.href;
+let inYoutube = activeUrl.includes('youtube');
+let isShorts = activeUrl.includes('shorts');
+
+function blockShorts(inYoutube, isShorts) {
+  if (inYoutube && !isShorts) {
+    try {
+      console.log('Hiding shorts tab');
+      shortsTab.style.display = 'none';
+      console.log('done hiding shorts tab');
+    } catch (error) {
+      console.log('An error occurred while trying to block the shorts tab. "YT shorts blocker"\nError: ' + error);
+    }
+    try {
+      var shortsSuggestions = document.getElementsByTagName('ytd-rich-section-renderer');
+      console.log('Hiding shorts suggestions');
+      for (let i = 0; i < shortsSuggestions.length; i++) {
+        shortsSuggestions[i].style.display = 'none';
+        console.log('hiding suggestion: ',i, shortsSuggestions[i]);
+      }
+      console.log('done hiding shorts suggestions');
+    } catch (error) {
+      console.log('An error occurred while trying to block the shorts suggestions. "YT shorts blocker"\nError: ' + error);
+    }
+  }
+  if (inYoutube && isShorts) {
+    try {
+      console.log('Blocking shorts');
+      bodyTag = document.querySelector('body');
+      bodyTag.innerHTML = 'You are watching a short! Shorts are blocked by YT Shorts Blocker <a href="https://www.youtube.com">Go back to YouTube</a>';
+      bodyTag.setAttribute('style', 'text-align: center; font-size: 20px; padding: 20px; color: #fff; background-color: #000; position: fixed; top: 0; left: 0; width: 100%; z-index:');
+      document.querySelector('title').innerText = 'Shorts Blocked';
+      let audio = new Audio();
+      audio.muted = true;
+    } catch (error) {
+      console.log('An error occurred while trying to block the shorts. "YT shorts blocker"\nError: ' + error);
+    }
+  }
+}
+
+blockShorts(inYoutube, isShorts);
+
+document.querySelector('*').addEventListener('click', function () {
+  setTimeout(() => {
+    activeUrl = window.location.href;
+    inYoutube = activeUrl.includes('youtube');
+    isShorts = activeUrl.includes('shorts');
+    blockShorts(inYoutube, isShorts);
+  }, 1000);
+});
